aefa32afba527d7ae77eaf87048fed00b22f5011
Paul Riddle <paulr@umbc.edu>
2022-04-19 16:53:23 -0400

    Simplify entrypoint config file substitution logic

--- universalforwarder/entrypoint.sh
+++ universalforwarder/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 #
-# Need to jump through some hoops here, because the base image creates a volume
-# for /opt/splunkforwarder/etc - so we can't ADD files there in the Dockerfile
+# Can't directly copy these to /opt/splunkforwarder/etc in the Dockerfile, because the Ansible
+# playbook will overwrite it when it populates the baked-in Docker volume for the first time.
 #
-if [ -d /tmp/local ]; then
-   /bin/sudo -u splunk sed -i "s/_SPLUNK_DEFAULT_HOSTNAME_/${SPLUNK_DEFAULT_HOSTNAME}/g" /tmp/local/inputs.conf
-   /bin/sudo -u splunk sed -i "s/_SPLUNK_PASS4SYMMKEY_/${SPLUNK_PASS4SYMKEY}/g" /tmp/local/server.conf
-   /bin/sudo -u splunk sed -i "s/_SPLUNK_SERVER_/${SPLUNK_SERVER}/g" /tmp/local/outputs.conf
-   /bin/sudo -u splunk cp -p /tmp/local/* /opt/splunkforwarder-etc/system/local
-   /bin/sudo -u splunk rm -rf /tmp/local
-fi
+local_dir=/opt/splunkforwarder-etc/system/local
+/bin/sudo -u splunk -- sh -s <<EOF
+sed -i "s/_SPLUNK_DEFAULT_HOSTNAME_/${SPLUNK_DEFAULT_HOSTNAME}/g" ${local_dir}/inputs.conf
+sed -i "s/_SPLUNK_PASS4SYMMKEY_/${SPLUNK_PASS4SYMKEY}/g" ${local_dir}/server.conf
+sed -i "s/_SPLUNK_SERVER_/${SPLUNK_SERVER}/g" ${local_dir}/outputs.conf
+EOF
 exec /sbin/entrypoint.sh $@
 exit 1
