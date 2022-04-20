eea7854caa2015c4076280b81b9507d78a4b6063
Paul Riddle <paulr@umbc.edu>
2022-04-20 13:39:25 -0400

    Don't do env variable substitutions more than once

--- universalforwarder/entrypoint.sh
+++ universalforwarder/entrypoint.sh
@@ -5,9 +5,11 @@
 #
 local_dir=/opt/splunkforwarder-etc/system/local
 /bin/sudo -u splunk -- sh -s <<EOF
-sed -i "s/_SPLUNK_DEFAULT_HOSTNAME_/${SPLUNK_DEFAULT_HOSTNAME}/g" ${local_dir}/inputs.conf
-sed -i "s/_SPLUNK_PASS4SYMMKEY_/${SPLUNK_PASS4SYMKEY}/g" ${local_dir}/server.conf
-sed -i "s/_SPLUNK_SERVER_/${SPLUNK_SERVER}/g" ${local_dir}/outputs.conf
+if /bin/grep -q _SPLUNK_DEFAULT_HOSTNAME_ ${local_dir}/inputs.conf; then
+    sed -i "s/_SPLUNK_DEFAULT_HOSTNAME_/${SPLUNK_DEFAULT_HOSTNAME}/g" ${local_dir}/inputs.conf
+    sed -i "s/_SPLUNK_PASS4SYMMKEY_/${SPLUNK_PASS4SYMKEY}/g" ${local_dir}/server.conf
+    sed -i "s/_SPLUNK_SERVER_/${SPLUNK_SERVER}/g" ${local_dir}/outputs.conf
+fi
 EOF
 exec /sbin/entrypoint.sh $@
 exit 1
